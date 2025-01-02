61ae2b76786ba57b4a104685cf630d0593b06fb7
zaejin-404 <zarjinislamjewel@gmail.com>
2025-01-02 13:03:03 +0600

    refactor app.js: standardize path import, update profile route to render view

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const express = require('express');
-const Path = require('path');
-
+const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(Path.join(__dirname, 'public')));
-
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('index');
 });
-app.get('/profile/:user', (req, res) => {
-  res.send('Profile Page');
+app.get('/profile/:username', (req, res) => {
+  res.render('profile', { username: req.params.username });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
