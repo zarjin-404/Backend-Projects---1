a7ded97a1ae10c122dc0e334fc467db5ffb1e6fb
zaejin-404 <zarjinislamjewel@gmail.com>
2025-01-02 18:07:01 +0600

    Add task CRUD, update views, remove unused CSS

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -9,10 +10,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index');
+  fs.readdir('./Task/', (err, files) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(files);
+      res.render('index', { files: files });
+    }
+  });
 });
-app.get('/profile/:username', (req, res) => {
-  res.send(`Profile, ${req.params.username}`);
+app.post('/create', (req, res) => {
+  fs.writeFile(
+    `./Task/${req.body.title.split(' ').join('')}.txt`,
+    req.body.description,
+    (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.redirect('/');
+      }
+    },
+  );
 });
 
 app.listen(port, () => {
