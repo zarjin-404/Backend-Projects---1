c43d2087debcee8948f161142530f011590a7d70
zaejin-404 <zarjinislamjewel@gmail.com>
2025-01-02 13:07:50 +0600

    fix: update profile route to send username as plain text and reorder static file middleware

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 app.get('/profile/:username', (req, res) => {
-  res.render('profile', { username: req.params.username });
+  res.send(`Profile, ${req.params.username}`);
 });
 
 app.listen(port, () => {
