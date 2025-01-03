e2ef3572f7f6f1224e0994a0c358a3098ace769f
zarjin-404 <zarjinislamjewel@gmail.com>
2025-01-03 17:32:34 +0600

    feat: implement task renaming functionality; add edits view and update routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
+const { render } = require('ejs');
 const app = express();
 const port = 3000;
 
@@ -41,6 +42,24 @@ app.get('/files/:filename', (req, res) => {
       res.render('show', { filename: req.params.filename, Data: data });
     }
   });
+});
+
+app.get('/edits/:filename', (req, res) => {
+  res.render('edits', { filename: req.params.filename });
+});
+
+app.post('/edits', (req, res) => {
+  fs.rename(
+    `./Task/${req.body.OldTitle}`,
+    `./Task/${req.body.NewTitle}`,
+    (err) => {
+      if (err) {  
+        console.log(err);
+      } else {
+        res.redirect('/');
+      }
+    },
+  );
 });
 
 app.listen(port, () => {
